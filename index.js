46abc2c7571d31372c34476dbbda6968bb28ec95
Luxy <luxydmr020904@163.com>
2024-07-18 15:02:36 +0800

    24/07/18

---
+++ index.js
@@ -0,0 +1,57 @@
+const newsletter = require('./util/newsletter')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+// 以下部分处理各种http请求
+app.get('/', (req, res) => {
+    const { openid } = req.query // 通过get参数形式指定openid
+    console.log("Request Content:", req)
+    res.send('欢迎使用微信云托管！')
+})
+
+app.get('/send', (req, res) => {
+    const { openid } = req.query // 通过get参数形式指定openid
+
+})
+
+app.post('/signup', async (req, res) => {
+    const {openid} = req.query // 通过get参数形式指定openid
+    console.log("Got a POST request from /signup path!")
+
+    const data = req.body
+    // let title = req.body.title
+    console.log("Corresponding title:", data)
+
+    const info = await newsletter.signupNotify(openid,
+        "活动标题",
+        "address",
+        "报名人",
+        "￥1",
+        "活动备注")
+    res.send(info)
+})
+
+app.post('/testpost', async (req, res) => {
+    const {openid} = req.query // 通过get参数形式指定openid
+    console.log("Got a POST request from /signup path!")
+
+    const data = req.body + openid
+    let title = req.body.title
+    console.log("Corresponding title:", title)
+
+    // const info = await newsletter.signupNotify("openid",
+    //     "活动标题",
+    //     "address",
+    //     "报名人",
+    //     "￥1",
+    //     "活动备注")
+    res.send(data)
+})
+
+// 以下部分监听接口
+const port = process.env.PORT || 80
+app.listen(port, () => {
+    console.log('服务启动成功，端口：', port)
+})
---
+++ util/newsletter.js
@@ -0,0 +1,89 @@
+const request = require("request");
+
+// 发送用户报名成功时候的通知订阅消息
+module.exports.signupNotify = async function signupSuccessNotify(_openid, _title, _address, _name, _amount, _memo) {
+    // return _title
+    return new Promise((resolve, reject) => {
+        request({
+            url: "http://api.weixin.qq.com/cgi-bin/message/subscribe/send",
+            method: "POST",
+            body: JSON.stringify({
+                touser: _openid,
+                template_id: "nJRikwbUcAe12oBPt5fJoxHmRKDZu6_WxCT2uUSF9rs",
+                miniprogram_state: "developer",
+                data: {
+                    // 这里替换成自己的模板ID的详细事项，不要擅自添加或更改
+                    // 按照key前面的类型，对照参数限制填写，否则都会发送不成功
+                    // 活动名称
+                    thing2: {
+                        value: _title
+                    },
+                    // 地址
+                    thing5: {
+                        value: _address
+                    },
+                    // 报名人
+                    thing23: {
+                        value: _name
+                    },
+                    // 费用合计
+                    amount18: {
+                        value: _amount
+                    },
+                    // 备注
+                    thing19: {
+                        value: _memo
+                    },
+                },
+            }),
+        },function(error,res){
+            if(error) reject(error)
+            resolve(res.body)
+        });
+    });
+};
+
+// 活动开始前的通知订阅消息
+module.exports.eventStartNotify = async function eventStartNotify(newsletter) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: "http://api.weixin.qq.com/cgi-bin/message/subscribe/send",
+            method: "POST",
+            body: JSON.stringify({
+                touser: openid,
+                template_id: "jpqYOzYeGO28PzAwO-4LZaTKylUEnSj-0_1tvKeGAAQ",
+                miniprogram_state: "developer",
+                data: {
+                    // 这里替换成自己的模板ID的详细事项，不要擅自添加或更改
+                    // 按照key前面的类型，对照参数限制填写，否则都会发送不成功
+                    //活动名称
+                    thing4: {
+                        value: "活动名称"
+                    },
+                    //开始时间
+                    date3: {
+                        value: new Date()
+                    },
+                    //活动地点
+                    thing6: {
+                        value: "活动地点"
+                    },
+                    //距离开始时间
+                    character_string17: {
+                        value: "距离开始时间"
+                    },
+                    //温馨提示
+                    thing7: {
+                        value: "温馨提示"
+                    },
+                },
+            }),
+        },function(error,res){
+            if(error) reject(error)
+            resolve(res.body)
+        });
+    });
+};
+
+// 创建的活动有人报名的通知订阅消息
+// 抽奖开奖的通知订阅消息
